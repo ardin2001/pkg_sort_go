b8de8af89129d69f3878320489fa77a3b4603d16
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2023-03-25 19:28:58 +0700

    update major upgrade

--- sortingArray.go
+++ sortingArray.go
@@ -2,7 +2,8 @@ package sortingArray
 
 import "fmt"
 
-func InsectionSort(numbers ...int) {
+// major upgrade adalah perubahan yg mejebabkan error atau perubahan dratis pada user yg menggunakan package, sebuah contoh pada parameter
+func InsectionSort(jmlh_data int, numbers ...int) {
 	for i := 1; i < len(numbers); i++ {
 		for j := i - 1; j >= 0; j-- {
 			if numbers[j+1] < numbers[j] {
@@ -11,7 +12,7 @@ func InsectionSort(numbers ...int) {
 		}
 	}
 
-	fmt.Println("=======proccess=======")
+	fmt.Println("=======proccess", jmlh_data, "=======")
 	fmt.Println(numbers)
 	fmt.Println("=======done=======")
 }
